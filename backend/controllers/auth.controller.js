032546e42ae5ddecffe19acb380d37f3db9922b1
Coangkuxm <coangkuxm@gmail.com>
2025-12-12 13:59:41 +0700

    update

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { query } = require('../config/db');
+const { query } = require('../../config/db');
 const { UnauthorizedError } = require('../utils/errors');
 
 const authController = {
