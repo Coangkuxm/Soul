775e77e64158615f0065cf9066d6206712323bf0
Coangkuxm <coangkuxm@gmail.com>
2025-12-30 20:57:11 +0700

    update1

--- backend/controllers/social.controller.js
+++ backend/controllers/social.controller.js
@@ -1,4 +1,4 @@
-const db = require('../config/db');
+const db = require('../config/db-connection');
 const { BadRequestError, UnauthorizedError, NotFoundError } = require('../utils/errors');
 
 class SocialController {
